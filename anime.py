e181010b5f6d0a28aab01194513e831b0a9f24c1
Jere Krischel <jere.h.krischel@kp.org>
2021-07-06 20:13:50 -0400

    jere: adding retry for first call

--- anime.py
+++ anime.py
@@ -211,6 +211,10 @@ def download(link, title, season, loc, subOrDub):
 
 			callParams =  subprocess.Popen('curl -s ' + link + '|grep download_video|perl -p -ne \'s/(.*)(download_video\()([^\)]*)(.*)/$3/g\'', shell=True, stdout=subprocess.PIPE).stdout
 			params = callParams.read().decode().replace("'","").replace("\n","").split(',')
+			while (len(params[1]) != 1): # retry getting params
+				print('Retrying...' + link )
+				callParams =  subprocess.Popen('curl -s ' + link + '|grep download_video|perl -p -ne \'s/(.*)(download_video\()([^\)]*)(.*)/$3/g\'', shell=True, stdout=subprocess.PIPE).stdout
+				params = callParams.read().decode().replace("'","").replace("\n","").split(',')
 
 			url = 'https://sbplay.org/dl?op=download_orig&id=' + params[0] + '&mode=' + params[1] + '&hash=' + params[2]
 			print('Url: ' + url)
