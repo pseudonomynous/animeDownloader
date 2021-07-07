0321d8e50c95e9c6853b72a3062b2b522893e829
Jere Krischel <jere.h.krischel@kp.org>
2021-07-07 12:33:15 -0400

    jere: fixing print statement

--- anime.py
+++ anime.py
@@ -214,7 +214,7 @@ def download(link, title, season, loc, subOrDub):
 			while (len(params[1]) != 1): # retry getting params
 				time.sleep(10)
 				print('Retrying params...' + link )
-				print('params: ' + params)
+				print(params)
 				callParams =  subprocess.Popen('curl -s ' + link + '|grep download_video|perl -p -ne \'s/(.*)(download_video\()([^\)]*)(.*)/$3/g\'', shell=True, stdout=subprocess.PIPE).stdout
 				params = callParams.read().decode().replace("'","").replace("\n","").split(',')
 
